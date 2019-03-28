f84702a9913f29324e5d298a80be4a7f2a974f14
lazarljubenovic <lazar.ljubenovic.1995@gmail.com>
2019-03-28 18:57:35 +0100

    Updates

---
+++ precache-manifest.1c3deca1a968f8dd8d7d1fa970005b2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "455b2f76bcb7cc6de835",
+    "url": "/loptice/static/js/runtime~main.84ed4c4f.js"
+  },
+  {
+    "revision": "44b87f9483626bc3bb2e",
+    "url": "/loptice/static/js/main.c20399bb.chunk.js"
+  },
+  {
+    "revision": "8644155d14d424d42f1e",
+    "url": "/loptice/static/js/2.76c767b8.chunk.js"
+  },
+  {
+    "revision": "44b87f9483626bc3bb2e",
+    "url": "/loptice/static/css/main.1a2b6395.chunk.css"
+  },
+  {
+    "revision": "8ff8abc527d78e8aecaa9847c60b8072",
+    "url": "/loptice/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/loptice/precache-manifest.1c3deca1a968f8dd8d7d1fa970005b2b.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/loptice/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
